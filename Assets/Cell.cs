b77a7bd022209ecc28ffaaad9c82dfe34e849087
MounirTakieddine <MounirTakieddine@bitbucket.org>
2018-04-04 19:24:28 +0100

    Initial repository upload

---
+++ Assets/Cell.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using UnityEngine;
+
+[Flags]
+public enum Walls
+{
+    NONE    = 0x00000000,
+    TOP     = 0x00000001,
+    RIGHT   = 0x00000002,
+    BOTTOM  = 0x00000004,
+    LEFT    = 0x00000008
+};
+
+[Flags]
+public enum VisitedFlags
+{
+    NONE    = 0x00000000,
+    VISITED = 0x00000001
+};
+
+public class Cell
+{
+    public Color        WallColor           { get; set; }
+    public Walls        CurrentWalls        { get; set; }
+    public VisitedFlags CurrentVisitedFlags { get; set; }
+    public int          Row                 { get; set; }
+    public int          Column              { get; set; }
+
+    public Cell(
+        int row,
+        int column,
+        Color wallColor
+    )
+    {
+        WallColor           = wallColor;
+        CurrentWalls        = Walls.NONE;
+        CurrentVisitedFlags = VisitedFlags.NONE;
+        Row                 = row;
+        Column              = column;
+    }
+
+    public bool IsVisited()
+    {
+        return (CurrentVisitedFlags & VisitedFlags.VISITED) == VisitedFlags.VISITED;
+    }
+
+    public void SetVisited()
+    {
+        CurrentVisitedFlags |= VisitedFlags.VISITED;
+    }
+
+    public void AddTopWall()
+    {
+        CurrentWalls |= Walls.TOP;
+    }
+
+    public void AddRightWall()
+    {
+        CurrentWalls |= Walls.RIGHT;
+    }
+
+    public void AddBottomWall()
+    {
+        CurrentWalls |= Walls.BOTTOM;
+    }
+
+    public void AddLeftWall()
+    {
+        CurrentWalls |= Walls.LEFT;
+    }
+
+    public void AddAllWalls()
+    {
+        AddTopWall();
+        AddRightWall();
+        AddBottomWall();
+        AddLeftWall();
+    }
+
+    public void RemoveTopWall()
+    {
+        CurrentWalls &= ~Walls.TOP;
+    }
+
+    public void RemoveRightWall()
+    {
+        CurrentWalls &= ~Walls.RIGHT;
+    }
+
+    public void RemoveBottomWall()
+    {
+        CurrentWalls &= ~Walls.BOTTOM;
+    }
+
+    public void RemoveLeftWall()
+    {
+        CurrentWalls &= ~Walls.LEFT;
+    }
+
+    public void RemoveAllWalls()
+    {
+        RemoveTopWall();
+        RemoveRightWall();
+        RemoveBottomWall();
+        RemoveLeftWall();
+    }
+
+    public void Render()
+    {
+        if ((CurrentWalls & Walls.TOP) == Walls.TOP)
+        {
+            Debug.DrawLine(new Vector3(Column, Row + 1), new Vector3(Column + 1, Row + 1), WallColor);
+        }
+
+        if ((CurrentWalls & Walls.RIGHT) == Walls.RIGHT)
+        {
+            Debug.DrawLine(new Vector3(Column + 1, Row + 1), new Vector3(Column + 1, Row), WallColor);
+        }
+
+        if ((CurrentWalls & Walls.BOTTOM) == Walls.BOTTOM)
+        {
+            Debug.DrawLine(new Vector3(Column + 1, Row), new Vector3(Column, Row), WallColor);
+        }
+
+        if ((CurrentWalls & Walls.LEFT) == Walls.LEFT)
+        {
+            Debug.DrawLine(new Vector3(Column, Row), new Vector3(Column, Row + 1), WallColor);
+        }
+    }
+};
---
+++ Assets/MazeGenerator.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGenerator : MonoBehaviour
+{
+    public Cell[][]         CellMatrix      { get; private set; }
+    public Stack<Cell>      BacktrackStack  { get; private set; }
+    public Cell             CurrentCell     { get; private set; }
+    private System.Random   randomizer;
+
+    public Color    MainCellColor, CurrentCellColor, VisitedCellColor;
+    public int      Rows, Columns;
+
+    void AllocateCellMatrix()
+    {
+        if (Rows <= 0 || Columns <= 0)
+        {
+            return;
+        }
+
+        randomizer  = new System.Random();
+        CellMatrix  = new Cell[Rows][];
+
+        for (int i = 0; i < Rows; ++i)
+        {
+            Cell[] cellRow = new Cell[Columns];
+
+            for (int j = 0; j < Columns; ++j)
+            {
+                Cell cell = new Cell(i, j, MainCellColor);
+                cell.AddAllWalls();
+                cellRow[j] = cell;
+            }
+
+            CellMatrix[i] = cellRow;
+        }
+    }
+
+    public bool MatrixContainsUnvisitedCells()
+    {
+        for (int i = 0; i < Rows; ++i)
+        {
+            for (int j = 0; j < Columns; ++j)
+            {
+                if (!CellMatrix[i][j].IsVisited())
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private void LoadNeighbors(
+        Cell cell,
+        List<Cell> neighbors
+    )
+    {
+        int cellRow             = cell.Row;
+        int cellColumn          = cell.Column;
+        int topNeighborRow      = cellRow + 1;
+        int bottomNeighborRow   = cellRow - 1;
+        int leftNeighborColumn  = cellColumn - 1;
+        int rightNeighborColumn = cellColumn + 1;
+
+        if (topNeighborRow < Rows)
+        {
+            neighbors.Add(CellMatrix[topNeighborRow][cellColumn]);
+        }
+
+        if (rightNeighborColumn < Columns)
+        {
+            neighbors.Add(CellMatrix[cellRow][rightNeighborColumn]);
+        }
+
+        if (bottomNeighborRow >= 0)
+        {
+            neighbors.Add(CellMatrix[bottomNeighborRow][cellColumn]);
+        }
+
+        if (leftNeighborColumn >= 0)
+        {
+            neighbors.Add(CellMatrix[cellRow][leftNeighborColumn]);
+        }
+    }
+
+    public static bool ContainsUnvisitedCells(List<Cell> cells)
+    {
+        for (int i = 0; i < cells.Count; ++i)
+        {
+            if (!cells[i].IsVisited())
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void RemoveWallsBetween(
+        Cell first,
+        Cell second
+    )
+    {
+        int rowDifference       = second.Row - first.Row;
+        int columnDifference    = second.Column - first.Column;
+
+        if (rowDifference > 0)
+        {
+            first.RemoveTopWall();
+            second.RemoveBottomWall();
+        }
+        else if (rowDifference < 0)
+        {
+            first.RemoveBottomWall();
+            second.RemoveTopWall();
+        }
+        else if (columnDifference > 0)
+        {
+            first.RemoveRightWall();
+            second.RemoveLeftWall();
+        }
+        else if (columnDifference < 0)
+        {
+            first.RemoveLeftWall();
+            second.RemoveRightWall();
+        }
+    }
+
+	void Awake ()
+    {
+        AllocateCellMatrix();
+        BacktrackStack  = new Stack<Cell>();
+        CurrentCell     = CellMatrix[0][0];
+        CurrentCell.SetVisited();
+    }
+	
+	void Update ()
+    {
+        if (MatrixContainsUnvisitedCells())
+        {
+            List<Cell> currentNeighbors = new List<Cell>();
+            LoadNeighbors(CurrentCell, currentNeighbors);
+
+            if (ContainsUnvisitedCells(currentNeighbors))
+            {
+                int neighborCount   = currentNeighbors.Count;
+                Cell neighbor       = currentNeighbors[randomizer.Next(0, neighborCount)];
+
+                BacktrackStack.Push(CurrentCell);
+
+                RemoveWallsBetween(CurrentCell, neighbor);
+
+                CurrentCell = neighbor;
+                CurrentCell.SetVisited();
+            }
+            else if (BacktrackStack.Count > 0)
+            {
+                Cell topCell    = BacktrackStack.Pop();
+                CurrentCell     = topCell;
+            }
+        }
+
+        for (int i = 0; i < Rows; ++i)
+        {
+            for (int j = 0; j < Columns; ++j)
+            {
+                Cell cell       = CellMatrix[i][j];
+                cell.WallColor  = cell == CurrentCell ? CurrentCellColor : (cell.IsVisited() ? VisitedCellColor : MainCellColor);
+                cell.Render();
+            }
+        }
+    }
+};
